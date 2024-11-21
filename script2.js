4f2687fe5ca2c73d7c365d86641f3abf1ced86ea
karolw881 <79966545+karolw881@users.noreply.github.com>
2024-11-21 02:27:59 +0100

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,8 @@
+import * as THREE from 'three';
+
+let camera, scene, renderer, group;
+let windowHalfX = window.innerWidth / 2;
+let windowHalfY = window.innerHeight / 2;
+
+let targetX = 0;
+let targetY = 0;
